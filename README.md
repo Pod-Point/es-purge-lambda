9576831aba4bae131ebe4bb80cd029b9b0945b64
threesquared <ben@3sq.re>
2018-04-12 16:16:50 +0100

    Begin function

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# es-purge-lambda
-Clean up old indices from AWS ES
+# slack-relay-lambda
+
+Lambda script written in TypeScript that cleans up old indices from AWS ES
+
+### Lambda functions
+
+* `purge` - Deletes old indices.
+
+### Config
+
+Edit your `.env.yml`
+
+### Deploy
+This function can be deployed with Serverless.
+
+* `npm run deploy`
---
+++ src/purge.ts
@@ -0,0 +1,37 @@
+import { Context, SNSEvent, Callback } from 'aws-lambda';
+import { Client } from 'elasticsearch';
+import * as moment from 'moment';
+import * as HttpAmazonESConnector from 'http-aws-es';
+
+/**
+ * Purge old ES indices.
+ *
+ * @param {SNSEvent} event
+ * @param {Context} context
+ * @param {Callback} callback
+ */
+export async function handler(event: SNSEvent, context: Context, callback: Callback): Promise<void> {
+    const client = new Client({
+        host: [ process.env.ES_ENDPOINT ],
+        connectionClass: HttpAmazonESConnector,
+    });
+
+    const purgeDate: string = moment().subtract(process.env.DAYS_TO_KEEP, 'd').format(process.env.DATE_FORMAT);
+
+    console.log(purgeDate);
+
+    const results = await client.cat.indices({
+        format: 'json',
+        index: `${process.env.INDEX_PREFIX}*`,
+    });
+
+    results.forEach((index: any) => {
+        const indexDate: string = moment(index.index.slice(process.env.INDEX_PREFIX.length)).format('YYYY-MM-DD')
+
+        if (indexDate < purgeDate) {
+            console.log(`Deleting: ${process.env.INDEX_PREFIX}${indexDate}`);
+        }
+    })
+
+    callback();
+}
