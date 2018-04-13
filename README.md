4b57c846a95af01cfce6f93f5a20b46f0cf9b26a
threesquared <ben@3sq.re>
2018-04-13 09:39:18 +0100

    Add linting

--- README.md
+++ README.md
@@ -4,11 +4,17 @@ Lambda script written in TypeScript that cleans up old indices from AWS ES
 
 ### Lambda functions
 
-* `purge` - Deletes old indices.
+* `purge` - Deletes old indices every day at 1:00 am
 
 ### Config
 
-Edit your `.env.yml`
+Edit your `.env.yml`. For example:
+
+```env
+INDEX_PREFIX: logstash-
+DAYS_TO_KEEP: 30
+DATE_FORMAT: YYYY-MM-DD
+```
 
 ### Deploy
 This function can be deployed with Serverless.
--- src/purge.ts
+++ src/purge.ts
@@ -12,26 +12,28 @@ import * as HttpAmazonESConnector from 'http-aws-es';
  */
 export async function handler(event: SNSEvent, context: Context, callback: Callback): Promise<void> {
     const client = new Client({
-        host: [ process.env.ES_ENDPOINT ],
+        host: [process.env.ES_ENDPOINT],
         connectionClass: HttpAmazonESConnector,
     });
 
     const purgeDate: string = moment().subtract(process.env.DAYS_TO_KEEP, 'd').format(process.env.DATE_FORMAT);
 
-    console.log(purgeDate);
-
-    const results = await client.cat.indices({
+    const results: IndexData[] = await client.cat.indices({
         format: 'json',
         index: `${process.env.INDEX_PREFIX}*`,
     });
 
-    results.forEach((index: any) => {
-        const indexDate: string = moment(index.index.slice(process.env.INDEX_PREFIX.length)).format('YYYY-MM-DD')
+    results.forEach((result: IndexData) => {
+        const indexDate: string = moment(result.index.slice(process.env.INDEX_PREFIX.length)).format('YYYY-MM-DD');
 
         if (indexDate < purgeDate) {
             console.log(`Deleting: ${process.env.INDEX_PREFIX}${indexDate}`);
         }
-    })
+    });
 
     callback();
+}
+
+interface IndexData {
+    index: string;
 }
